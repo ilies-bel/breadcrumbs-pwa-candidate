e230e17e096fa4e97bf04b24eaa6d36f4faeddf6
ilies-bel <beldjilali.ilies@gmail.com>
2021-01-23 13:46:50 +0100

    Styling bottom app bar

--- src/components/Navigation/bottomNavigation.js
+++ src/components/Navigation/bottomNavigation.js
@@ -11,16 +11,21 @@ import ListAltOutlinedIcon from '@material-ui/icons/ListAltOutlined';
 import {BottomNavigation, BottomNavigationAction, makeStyles} from "@material-ui/core";
 import {compose} from "recompose";
 
-const useStyles = makeStyles({
-    root: {
+
+const useStyles = makeStyles((theme) => ({
+    BottomNavigation: {
+        bottom: theme.spacing(2),
+        position: 'fixed'
+    },
+    BottomNavigationAction: {
         minWidth: "20%",
         border: 1.5
-
     },
-});
+}));
+
 
 const BottomNav = () => {
-    const classes = useStyles();
+    const classes =  useStyles();
 
     const [value, setValue] = React.useState(0);
 
@@ -32,26 +37,24 @@ const BottomNav = () => {
     };
 
     return (
-        <header>
             <BottomNavigation
+                className={classes.BottomNavigation}
                 value={value}
                 onChange={handleChange}
                 showLabels
             >
-                <BottomNavigationAction className={classes.root} value={ROUTES.HIRING_PROCESS} label={ROUTES.HIRING_LABEL}
+                <BottomNavigationAction className={classes.BottomNavigationAction} value={ROUTES.HIRING_PROCESS} label={ROUTES.HIRING_LABEL}
                                         icon={<ListAltOutlinedIcon/>}/>
-                <BottomNavigationAction className={classes.root} value={ROUTES.TIPS} label={ROUTES.TIPS_LABEL}
+                <BottomNavigationAction className={classes.BottomNavigationAction} value={ROUTES.TIPS} label={ROUTES.TIPS_LABEL}
                                         icon={<InfoOutlinedIcon/>}/>
-                <BottomNavigationAction className={classes.root} value={ROUTES.OFFICE} label={ROUTES.OFFICE_LABEL}
+                <BottomNavigationAction className={classes.BottomNavigationAction} value={ROUTES.OFFICE} label={ROUTES.OFFICE_LABEL}
                                         icon={<LocationOnOutlinedIcon/>}/>
-                <BottomNavigationAction className={classes.root} value={ROUTES.AMBASSADORS} label={ROUTES.AMBASSADORS_LABEL}
+                <BottomNavigationAction className={classes.BottomNavigationAction} value={ROUTES.AMBASSADORS} label={ROUTES.AMBASSADORS_LABEL}
                                         icon={<SchoolOutlinedIcon/>}/>
-                <BottomNavigationAction className={classes.root} value={ROUTES.SOCIAL} label={ROUTES.SOCIAL_LABEL}
+                <BottomNavigationAction className={classes.BottomNavigationAction} value={ROUTES.SOCIAL} label={ROUTES.SOCIAL_LABEL}
                                         icon={<ShareOutlinedIcon/>}/>
 
             </BottomNavigation>
-        </header>
-
 
     );
 
