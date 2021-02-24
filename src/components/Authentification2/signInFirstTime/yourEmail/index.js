ee043bf1823567ec6c5add1ec38d8fb1b74c0112
mmze <muigni-hazi.mze@sword-group.com>
2021-02-24 18:02:34 +0100

    Refactor

---
+++ src/components/Authentification2/signInFirstTime/yourEmail/index.js
@@ -0,0 +1,64 @@
+import React, {useState} from "react";
+import {FlashyButton, PageDescription} from "Navigation";
+import {ArrowRightAltOutlined} from "@material-ui/icons";
+import {useHistory, Link} from "react-router-dom";
+import * as ROUTES from 'constants/routes';
+import {loginRequest, registrationRequest} from "utils/axiosRequest6";
+import {useAuthContext} from "components/Authentification2/context";
+
+
+export const FormEmailPage = () => {
+    const history = useHistory();
+    const [email, setEmail] = useState(null);
+    const [password, setPass] = useState(null);
+
+    const [formData, setData] = useState({});
+    const [error, setError] = useState(false)
+    const context = useAuthContext();
+    let storage = window.localStorage;
+
+    const changeEmailEvent = (input) => {
+        setEmail(input)
+    }
+    const changePassEvent = (input) => {
+        setPass(input)
+    }
+
+    const handleChange = () => {
+        setData({
+            user: {
+                email: email,
+                password: password,
+                first_name: "Juki",
+            }
+        })
+    }
+    const sendRegistration = () => {
+        registrationRequest(formData).then(res => {
+            history.push("/tips", res.data);
+        })
+    }
+
+    
+    return (
+        <div>
+            <PageDescription>First, tell us who you are</PageDescription>
+
+                <input type="text" placeholder="your Email" aria-label="firstname"
+                       onChange={(event) => {
+                           changeEmailEvent(event.target.value);
+                           handleChange()
+                       } } />
+                <input type="password" placeholder="choose your password" aria-label="password"
+                       onChange={(event) => {
+                           changePassEvent(event.target.value);
+                           handleChange()
+                       } }/>
+                <input type="password" placeholder="confirm your password" aria-label="confirm-password"/>
+
+                <input type="checkbox" /> <label>I agree to receive mail notification (checkbox inutilisable) </label>
+                <FlashyButton onClick={ async() => await sendRegistration2() }  > Finish to sign in <ArrowRightAltOutlined/> </FlashyButton>
+
+        </div>
+    )
+}
