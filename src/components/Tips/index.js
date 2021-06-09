6d5cb5b50cfdb0a632fdfd1b02926d5d3e14d2fa
ilies-bel <beldjilali.ilies@gmail.com>
2021-06-10 00:01:58 +0200

    Figma compliant interview tips

--- src/components/Tips/index.js
+++ src/components/Tips/index.js
@@ -1,19 +1,24 @@
 import React from 'react';
 import {TitleSource} from 'Navigation/titleContext'
-import { TitleDescriptionSource } from 'Navigation/descriptionContext';
+import {TitleDescriptionSource} from 'Navigation/descriptionContext';
 import {TIPS_TITLE} from "constants/routes";
-import { TIPS_DESCRIPTION } from "constants/description"
+import {TIPS_DESCRIPTION} from "constants/description"
 
-import { makeStyles } from '@material-ui/core/styles';
+import {makeStyles} from '@material-ui/core/styles';
 import Accordion from '@material-ui/core/Accordion';
 import AccordionSummary from '@material-ui/core/AccordionSummary';
 import AccordionDetails from '@material-ui/core/AccordionDetails';
 import CircularProgress from '@material-ui/core/CircularProgress';
 import ExpandMore from '@material-ui/icons/ExpandMore';
-import { useGetTips } from 'utils/axios';
+import {useGetTips} from 'utils/axios';
 import {PageDescription} from "../Navigation";
+import {Grid} from "@material-ui/core";
+import Avatar from "@material-ui/core/Avatar";
 
 const useStyles = makeStyles((theme) => ({
+    text: {
+        fontFamily: 'Roboto',
+    },
     num: {
         width: '20px',
         height: '20px',
@@ -23,9 +28,10 @@ const useStyles = makeStyles((theme) => ({
         padding: '3px',
         marginRight: '10px',
         color: '#3572F1',
+        fontFamily: 'Quicksand',
     },
     nested: {
-      paddingLeft: theme.spacing(10),
+        paddingLeft: theme.spacing(10),
     },
     BottomBorder: {
         width: "100%",
@@ -35,25 +41,43 @@ const useStyles = makeStyles((theme) => ({
 }))
 
 const Tips = () => {
-    const [{ data, loading, error }, refetch] = useGetTips();
+    const [{data, loading, error}, refetch] = useGetTips();
     const [open, setOpen] = React.useState(true);
 
     const classes = useStyles();
 
-    if (loading) return <CircularProgress />
+    if (loading) return <CircularProgress/>
     if (error) return <strong>Error. No data found</strong>
 
     return (
         <>
             <TitleSource>{TIPS_TITLE}</TitleSource>
-            <div>
+            <PageDescription>
                 {TIPS_DESCRIPTION}
-            </div>
-            
-                {
+            </PageDescription>
+
+            {
                 data.map((tips, index) =>
                     <Accordion key={index}>
-                       <AccordionSummary > <span className={classes.num}>{index+1}</span> {tips.title} <ExpandMore /></AccordionSummary>
+                        <AccordionSummary>
+
+                            <Grid container>
+                                <Grid item xs={2}>
+                                    <span className={classes.num}>{index + 1}</span>
+                                </Grid>
+
+                                <Grid item xs={8}>
+                                    <p className={classes.text}>{tips.title}</p>
+
+                                </Grid>
+
+                                <Grid item xs={2}>
+                                    <ExpandMore/>
+                                </Grid>
+                            </Grid>
+
+                        </AccordionSummary>
+
                         <AccordionDetails>
                             <div>
                                 {tips.description}
@@ -61,8 +85,8 @@ const Tips = () => {
 
                         </AccordionDetails>
                     </Accordion>
-                    )
-                }
+                )
+            }
         </>
 
     );
