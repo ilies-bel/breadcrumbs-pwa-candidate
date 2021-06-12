b28a746392a14a199e565ca2bafe50865f5628b2
ilies-bel <beldjilali.ilies@gmail.com>
2021-06-12 22:28:48 +0200

    Fix mailto

--- src/components/Ambassadors/AmbassadorList.js
+++ src/components/Ambassadors/AmbassadorList.js
@@ -6,6 +6,7 @@ import { PageDescription } from '../Navigation';
 
 import Avatar from '@material-ui/core/Avatar';
 import { Label } from '@material-ui/icons';
+import "./ambassador.scss"
 
 const collaborators = [
     {
@@ -13,6 +14,7 @@ const collaborators = [
         photo: "https://upload.wikimedia.org/wikipedia/commons/0/04/Elon_Musk_and_Hans_Koenigsmann_at_the_SpaceX_CRS-8_post-launch_press_conference_%2826223624532%29_%28cropped%29.jpg",
         catch_phrase: "Strenght, Health and Futur",
         description: "Administration",
+        mail: "iliesb.pro@gmail.com",
         pronom: "him"
     },
     {
@@ -20,6 +22,7 @@ const collaborators = [
         photo: "https://s1.qwant.com/thumbr/0x380/8/c/0cd410d28660781ca6b2920f52a271d8c15617ea12a1ca0cfbf198b02fff8b/1200px-20th_Anniversary_Schwab_Foundation_Gala_Dinner_%2844887783681%29.jpg?u=https%3A%2F%2Fupload.wikimedia.org%2Fwikipedia%2Fcommons%2Fthumb%2F1%2F1d%2F20th_Anniversary_Schwab_Foundation_Gala_Dinner_%252844887783681%2529.jpg%2F1200px-20th_Anniversary_Schwab_Foundation_Gala_Dinner_%252844887783681%2529.jpg&q=0&b=1&p=0&a=0",
         catch_phrase: "Smile and live !",
         description: "Human Ressource.",
+        mail: "iliesb.pro@gmail.com",
         pronom: "him"
     },
     {
@@ -27,11 +30,15 @@ const collaborators = [
         photo: "https://www.gewoonvoorhem.nl/app/uploads/2020/01/Jason-Statham-Countdown-Rep-e1578660419114.jpg",
         catch_phrase: "Rule number 1 : No question",
         description: "Taxi Driver. Delivery service",
+        mail: "iliesb.pro@gmail.com",
         pronom: "him"
     }
 ]
 
 const AmbassadorItem = (props) => {
+
+    let mailto=`mailto:${props.mail}?subject=Question sur Breadcrumbs&cc=partenaire@breadcrumbs.net&body=Bonjour,`
+
     return (
     <>
     <div className="avatar">
@@ -40,7 +47,8 @@ const AmbassadorItem = (props) => {
             <h3 className="title">{props.name}</h3>
             <label>{props.description}</label>
             <div className="catchPhrase">{props.catchPhrase}</div>
-            <span className='getTouch'>___Get in touch with {props.pronom}</span>
+            <a className='getTouch' href={mailto}>
+                ___Get in touch with {props.pronom}</a>
         </div>
     </div>
     </>
@@ -51,13 +59,13 @@ const AmbassadorList = () => {
     return (
         <>
             <div className='searchForm'>
-                <input type='text' placeholder='Search by keyword' aria-label='Search by keyword'/>
-                <select name='filterType' type='text' aria-label='Filter by profile type'>
+                               <select name='filterType' type='text' aria-label='Filter by profile type'>
                     <option value='' selected>Filter by profile type</option>
                     <option value='rh'>Human ressources</option>
                     <option value='manager'>Marketing Manager</option>
                 </select>
             </div>
+
             <div className="ambassadors">
             {
                 collaborators.map((collaborator, index) =>
